9bc67296f1aab41e697758a2bc404ece98737f96
sowjanya1104 <sowjanya1104@gmail.com>
2024-04-19 10:37:29 -0700

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Javagit
+# Javagit
---
+++ src/programs/FirstProgram.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		int a=10,b=20,sum;
+	     sum=a+b;
+		System.out.println("the addition is"+sum);
+		
+
+	}
+
+}
